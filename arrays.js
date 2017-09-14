e056004fde1569ea7ce933a0fbceb9991adf0d18
Harold Yang <harold.h.yang@gmail.com>
2017-09-14 16:38:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,6 +33,6 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-  array.slice (1)
+  array = array.slice (1)
   return array
 }
