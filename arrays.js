7a2ef250423cc0c1dc154843111840ca1751e9c4
Harold Yang <harold.h.yang@gmail.com>
2017-09-14 16:53:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,3 +41,8 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   array.pop();
   return array;
 }
+
+function removeElementFromEndOfArray (array) {
+  array = array.shift(0, array.length - 1);
+  return array;
+}
