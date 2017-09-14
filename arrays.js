ea75c3c0b714d7dfe4f34aaa2e014741a8377806
Harold Yang <harold.h.yang@gmail.com>
2017-09-14 16:07:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,9 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray (array, element) {
   var array = [`${element}`,...array];
+  return array;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  var array = [`${element}`,...array];
 }
