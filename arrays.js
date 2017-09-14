d8cfcf554cf2e1262ef617920336f3aee5722a4e
Harold Yang <harold.h.yang@gmail.com>
2017-09-14 16:10:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,5 +8,5 @@ function addElementToBeginningOfArray (array, element) {
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
   array = [`${element}`,...array];
-  return array
+  return array;
 }
