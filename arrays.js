d5d9c31da0f0782b9e42ac8088b24794dd780b21
Harold Yang <harold.h.yang@gmail.com>
2017-09-14 16:22:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,14 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   array.unshift (`${element}`); 
   return array;
 }
+
+
+function addElementToEndOfArray (array, element) {
+  var array = [...array, `${element}`];
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push (`${element}`); 
+  return array;
+}
